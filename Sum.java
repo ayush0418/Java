3c18a9c86e5fe77a8d19269133b619cc4d3d1114
ayush0418 <53502082+ayush0418@users.noreply.github.com>
2021-01-28 00:09:49 +0530

    Sum of 2 numbers

---
+++ Sum.java
@@ -0,0 +1,11 @@
+public class Sum
+{
+    public static void main(String[] args)
+    {
+        int a = 12;
+        int b = 10;
+        int c = 8;
+        int sum = a+b+c;
+        System.out.println(sum);
+    }
+}
