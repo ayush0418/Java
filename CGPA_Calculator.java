80aee270ff31b8797b58cf010fccce06ed4d45ec
ayush0418 <53502082+ayush0418@users.noreply.github.com>
2021-01-28 00:06:53 +0530

    Calculating CGPA out of 10

---
+++ CGPA_Calculator.java
@@ -0,0 +1,12 @@
+public class  CGPA_Calculator
+{
+    public static void main(String[] args)
+    {
+        float sub1 = 68;
+        float sub2 = 93;
+        float sub3 = 80;
+        float sub4 = 77;
+        float cgpa = (sub1+sub2+sub3+sub4)/40;
+        System.out.println(cgpa);
+    }
+}
