e4fadf4fa4fabf265a6ca0efb45d2774a576f06c
ayush0418 <53502082+ayush0418@users.noreply.github.com>
2021-01-28 00:09:23 +0530

    Different types of string commands

---
+++ String_Methods.java
@@ -0,0 +1,41 @@
+public class String_Methods
+{
+    public static void main(String[] args)
+    {
+        String name = "Ayush";
+        System.out.println(name);                    // Prints String
+
+        int value = name.length();
+        System.out.println(value);                   // prints the length of String
+
+        String lstring = name.toLowerCase();
+        System.out.println(lstring);                 // prints the string in lower case
+
+        String ustring = name.toUpperCase();
+        System.out.println(ustring);                 // prints the string in upper case
+
+        System.out.println(name.substring(3));       // prints substring from start to end [3 is starting point]
+        System.out.println(name.substring(1,4));     // prints substring from start to end {end point is not included}
+                                                     // [1 is starting point and 4 is ending point]
+
+        System.out.println(name.replace('A', 'P'));   // replace the char in string with new one
+        System.out.println(name.replace("A","Pi"));  // replace the string with new values.
+
+        System.out.println(name.startsWith("Ay"));  // returns true if the string starts with "A" and false if not.
+        System.out.println(name.endsWith("h"));    // returns true if the string ends with "h" and false if not.
+
+        System.out.println(name.charAt(2));       // returns the char at index 2
+
+        String modifiedName = "AyushAyush";
+        System.out.println(modifiedName.indexOf("y"));    // returns the first index of the given string starting from first
+        System.out.println(modifiedName.indexOf("y",4));  //returns the index of given string starting from index 4
+
+        System.out.println(modifiedName.lastIndexOf("y"));       // returns the first index of the given string starting from last
+        System.out.println(modifiedName.lastIndexOf("y",4));  // returns the index of given string within 4 index
+
+        System.out.println(name.equals("Ayush"));    // returns true if the string alphabet and case matches [String is case sensitive]
+        System.out.println(name.equalsIgnoreCase("aYusH")); // returns true if the string alphabet matches. ignore case sensitive problem
+
+    }
+
+}
