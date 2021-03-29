dcce79c44d4a6410603df0b2035bbcd0e61051d3
Ayush Bansal <bayush611@gmail.com>
2021-03-29 16:57:42 +0530

    Package 2

---
+++ Access_Modifiers/Within_Package/withinPackage.java
@@ -0,0 +1,8 @@
+package Within_Package;
+
+public class withinPackage {
+    public int rollNo = 1804;
+    private int age= 19;
+    String Name = "Ayush";
+    protected String Branch = "Devops";
+}
