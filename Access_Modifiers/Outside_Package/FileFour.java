c34b8e1ff9dbbb863252c698ed0b5bdc6be58dee
Ayush Bansal <bayush611@gmail.com>
2021-03-29 16:59:59 +0530

    Access Modifiers used outside the package

---
+++ Access_Modifiers/Outside_Package/FileFour.java
@@ -0,0 +1,15 @@
+package Outside_Package;
+import AccessModifiers.*;
+
+public class FileFour {
+    public static void main(String[] args) {
+
+        Modifiers obj3 = new Modifiers();
+
+//        System.out.println("Private age: " + obj3.age);
+//        System.out.println("Default Name" + obj3.Name);
+//        System.out.println("Protected: " + obj3.Branch);
+        System.out.println("Public: "+ obj3.rollNo );
+
+    }
+}
