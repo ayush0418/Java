6b6b487382686e6caad925ac52da71a53be49647
Ayush Bansal <bayush611@gmail.com>
2021-04-21 18:10:09 +0530

    Use of different methods in threading

--- Multithreading/methods.java
+++ Multithreading/methods.java
@@ -20,7 +20,7 @@ class Thread2 extends Thread{
 }
 
 
-public class Methods {
+public class methods {
     public static void main(String[] args) {
         Thread1 t1 = new Thread1();
         Thread2 t2 = new Thread2();
