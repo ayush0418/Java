478757a3b7c45fdda4f50dff3070fe5883fc3c74
Ayush Bansal <bayush611@gmail.com>
2021-06-01 21:23:19 +0530

    Connection of jdbc by using mysql

---
+++ JDBC/JdbcConnection.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class JdbcConnection {
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/JDBC_exp1","root","Ayush@0219");
+
+            String g = "create table Employee(id int(10) primary key , name varchar(30) not null, salary int(10), phone int(12), allowance int(10))";
+
+            Statement stmt = con.createStatement();
+            stmt.execute(g);
+
+            System.out.println("table created in Database....");
+            con.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
